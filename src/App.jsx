637d1ce5adee219dc839b95cb646758d39fcf6b4
Matiasalbina <154567225+Matiasalbina@users.noreply.github.com>
2024-05-03 16:06:26 -0400

    Primer avance desafio base de datos colaboradores

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react'
+import './App.css'
+
+import BaseColaboradores from './database/data'
+import { RegisterForm } from './components/Formulario';
+import { TableUsers } from './components/Listado';
+import { Search } from './components/Buscador';
+
+function App() {
+	const [data, setData] = useState(BaseColaboradores);
+	const [filteredData, setFilteredData] = useState([]);
+
+	const [form, setForm] = useState({
+		id: '',
+		nombre: '',
+		correo: '',
+		edad: '',
+		cargo: '',
+		telefono: '',
+	});
+
+	const [formState, setFormState] = useState('');
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const id = data.length + 1;
+		const { nombre, correo, edad, cargo, telefono } = event.target;
+
+		if (
+			!nombre.value ||
+			!correo.value ||
+			!edad.value ||
+			!cargo.value ||
+			!telefono.value
+		) {
+			setFormState('Todos los campos son requeridos');
+		} else {
+			const formData = {
+				id,
+				nombre: nombre.value,
+				correo: correo.value,
+				edad: edad.value,
+				cargo: cargo.value,
+				telefono: telefono.value,
+			};
+
+			handleAddUser(formData);
+		}
+	};
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+		setForm({
+			...form,
+			[name]: value,
+		});
+	};
+
+	const handleAddUser = (newUser) => {
+		setData([...data, newUser]);
+	};
+
+	const handleDelete = (id) => {
+		setData(data.filter((user) => user.id != id));
+	};
+
+	const handleSearch = (event) => {
+		const value = event.target.value.toLowerCase();
+
+		setFilteredData(
+			data.filter((user) =>
+				Object.values(user).some((userField) =>
+					userField.toLowerCase().includes(value)
+				)
+			)
+		);
+	};
+
+	return (
+		<>
+			<h1>APP.JSX</h1>
+
+			<RegisterForm
+				onSubmit={handleSubmit}
+				onChange={handleChange}
+			/>
+			<Search onChange={handleSearch} />
+			<TableUsers
+				users={data}
+				onDelete={handleDelete}
+				filteredData={filteredData}
+			/>
+		</>
+	);
+}
+
+export default App;
